c0906d11499fafa06725d411204daeb9c02034a8
Kaden <kna71@sfu.ca>
2026-05-31 19:42:25 -0700

    real lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  printf("a");
+  char path[256];
+
+  while (1) {
+    printf("Enter programs to run.\n> ");
+    if (fgets(path, sizeof(path), stdin) == NULL)
+      break;
+
+    path[strcspn(path, "\n")] = 0;
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+      perror("Fork failed");
+    } else if (pid == 0) {
+      execl(path, path, NULL);
+
+      printf("Exec failure\n");
+      exit(1);
+    } else {
+      waitpid(pid, NULL, 0);
+    }
+  }
   return 0;
 }
